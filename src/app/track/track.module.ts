c7d095d8a802e59676d03519a1d6bbd469f2363c
Anagha <cranagh1@in.ibm.com>
2019-02-05 10:44:36 +0530

    second commit

--- src/app/track/track.module.ts
+++ src/app/track/track.module.ts
@@ -4,9 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { SearchComponent } from './header/search/search.component';
+import { CardComponent } from './card/card.component';
+import { CardComponentComponent } from './card-component/card-component.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, WishlistComponent, SearchComponent],
+  declarations: [HeaderComponent, FooterComponent, WishlistComponent, SearchComponent, CardComponent, CardComponentComponent],
   imports: [
     CommonModule
   ],
